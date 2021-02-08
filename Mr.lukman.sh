308b70b05afe5cbed458544fd2fec71a8764dbf3
Mr.lukman <77709150+CYBERX507X@users.noreply.github.com>
2021-02-08 09:25:43 +0000

    Create Mr.lukman.sh

---
+++ Mr.lukman.sh
@@ -0,0 +1,244 @@
+#HANYA ORANG GOBLOK
+#YANG SUKA RECODE ORANG KAYA GK BISA MEMPROGAM 
+#SEMOGA MAM KAU MATI,RECODE INI ASU
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 meizu.py
+echo
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v("   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo $pur"==================="$cy" =============================Ôûê"
+sleep 3
+echo $i"Ôûê"$pu"  AUTHOR    :  Mr.Lukman  "$i"               Ôûê"
+sleep 4
+echo $i"Ôûê"$pu"  WhatsApp  :  08895422818801"$i"            Ôûê"
+sleep 5
+echo $i"Ôûê"$pu"  TIM       :  3R0R S4ST6M"$i"               Ôûê"
+sleep 6
+echo $i"Ôûê"$pu"  NAMA      :  CYBER X507X "$i"              Ôûê"
+sleep 5
+echo $i"Ôûê"$pu"  GITHUB    :  CYBER X507X"$i"               Ôûê"
+sleep 4
+echo $pur"==================="$cy" ============================="
+sleep 4
+echo
+echo $pur"==================="$cy" ============================="
+echo $me"SECRIPT INI PUNYA MR.Lukman"
+sleep 8
+echo $me"WA AREYU ANONYMOS"
+sleep 8
+echo $me"WA AREYU CYBER"
+sleep 8
+echo $me"CYBER PEMALANG INDONESIA"
+sleep 8
+echo $me"CYBER PRMALANG ARMY"
+sleep 8
+echo $pur"==================="$cy" ============================="
+echo $pur"==============="$cy"==================="
+echo $1"["$me"1"$ku"]"$i"[[ Temas (Indonesia) ]]           $me"[ "ON" ]
+echo $pur"==============="$cy"==================="
+sleep 3
+echo $i"["$me"2"$ku"]"$i"[[ PRENIUM FB ]]                  $me"[ "ON" ]
+echo $pur"==============="$pu"==================="
+sleep 3
+echo $i"["$me"3"$ku"]"$i"[[ BRUTE FORCE FB ]]              $me"[ "ON" ]
+echo $pur"==============="$pu"==================="
+sleep 7
+echo $i"["$me"4"$ku"]"$i"[[TARGGET  iSTRAGAM ]]            $me"[ "ON" ]
+echo $pur"==============="$bi"==================="
+sleep 7
+echo $i"["$me"5"$ku"]"$i"[[ SPM SMS ]]                     $me"[ "ON" ]
+echo $pur"==============="$cy"==================="
+sleep 7
+echo $i"["$me"6"$ku"]"$i"[[ SPM CALL ]]                    $me"[ "ON" ]
+echo $pur"==============="$cy"==================="
+sleep 7
+echo $i"["$me"7"$ku"]"$i"[[ Spm WatssAp ]]                 $me"[ "ON" ]
+echo $pur"==============="$pu"==================="
+sleep 7
+echo $i"["$me"8"$ku"]"$i"[[ DDOS Ters4K17i ]]              $me"[ "ON" ]
+echo $pur"==============="$pu"==================="
+sleep 7
+echo $i"["$me"00"$i"]"$ku" *EXIT*"
+echo
+echo $pur"Ôò¡ÔöÇ["$pur"Masukkan pilihan anda"$pur"]"
+read -p"Ôò¦ÔöÇ>> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 4
+pkg install python2
+pkg install git
+git clone https://github.com/CYBERX507X/temas
+cd temas
+python2 TEMA.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 4
+pkg install python2
+pkg install git
+git clone https://github.com/CYBERX507X/Fb0
+cd Fb0
+python2 Blackhat.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 5
+git clone https://github.com/CYBERX507X/Fbv1
+cd fb1
+python2 VIP.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 4
+pkg install git
+pkg install python2
+tor
+git clone https://github.com/CYBERX507X/ig
+cd ig
+bash Target_Ig.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 5
+git https://github.com/CYBERX507X/spm71
+cd spm71
+python2 Sms.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/CYBERX507X/spm1
+cd spm1
+python2 Panggilan.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/CYBERX507X/spm
+cd spm
+python2 Wa.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 6
+git clone https://github.com/CYBERX507X/Ters4K17i
+cd Ters4K17i
+python2 Ters4K17i.py
+fi
+
+if [ $pil = 0 ]
+then
+clear
+figlet -f slant "W A I T"|Ulolcat
+sleep 2
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
+sleep 2
+exit
+fi
